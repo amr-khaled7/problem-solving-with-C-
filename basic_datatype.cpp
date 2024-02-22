075d7e677ea8a21d84ea1ea0a202dd598403170a
amr-khaled7 <155344796+amr-khaled7@users.noreply.github.com>
2024-02-22 18:50:42 +0200

    Create basic_datatype.cpp

---
+++ basic_datatype.cpp
@@ -0,0 +1,22 @@
+// basic data type
+#include <iostream>
+using namespace std;
+int main()
+{
+    int x;
+    long long y;
+    char ch;
+    float f;
+    double d;
+    cin >> x ;
+    cin>> y;
+    cin >> ch;
+    cin >> f;
+    cin >> d;
+    cout << x << endl;
+    cout << y << endl;
+    cout << ch << endl;
+    cout << f << endl;
+    cout << d << endl;
+    return 0;
+}
